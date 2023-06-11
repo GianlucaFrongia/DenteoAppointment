ab1d72e19fcf4cf999aef907cc702af27d2d02bc
Gianluca Frongia <gianluca.frongia@students.fhnw.com>
2023-06-11 11:38:58 +0200

    github action setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   modules: [
     [
       '@pinia/nuxt',
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
         autoImports: ['defineStore', 'acceptHMRUpdate'],
       },
     ],
-  ],  
+  ],
   devtools: { enabled: true },
   imports: {
     dirs: ['stores'],
